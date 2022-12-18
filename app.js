7b5889d257bb4946196923a3611a110d45321ab0
sabrina <sabrinaabrodi20@gmail.com>
2022-12-18 09:00:44 +0100

    feat: selecting required elements

---
+++ app.js
@@ -0,0 +1,3 @@
+const statement = document.getElementById("statement");
+const optionButtons = document.querySelector("#options").children;
+const explanation = document.getElementById("explanation");

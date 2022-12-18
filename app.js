edc045ffac587ed1e279fe6ddc67d24f9129f18c
sabrina <sabrinaabrodi20@gmail.com>
2022-12-18 16:37:20 +0100

    feat: creating variable

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const statement = document.getElementById("statement");
 const optionButtons = document.querySelector("#options").children;
 const explanation = document.getElementById("explanation");
+
+const fact = {
+  statement: "Sweden has a royal Family.",
+  answer: true,
+  explanation: "Yes they do.",
+};
+
+statement.textContent = fact.statement;
